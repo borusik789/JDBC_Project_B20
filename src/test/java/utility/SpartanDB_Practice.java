f5fec3172e3e45f1113fabf6423c0c2d30a14011
Akbar <Akbar@cybertekschool.com>
2020-11-28 14:47:25 -0500

    JDBC Day4 - Added new createConncetion method with 3 parameters

---
+++ src/test/java/utility/SpartanDB_Practice.java
@@ -0,0 +1,20 @@
+package utility;
+
+public class SpartanDB_Practice {
+
+    // create a main method
+    // call your create connection method with 3 params
+    // provide same connection string as HR
+    // provide username and password as SP SP
+    // run query SELECT * FROM SPARTANS
+    // call few methods from DB_Utility
+
+    // after you are done with this :
+    // now add below to your properties file
+    // spartan.database.url=jdbc:oracle:thin:@YOUR_IP_HERE:1521:XE
+    // spartan.database.username=SP
+    // spartan.database.password=SP
+
+    // REPLACE YOUR HARDCODED CONNECTION INFO IN THIS CLASS WITH PROPERTIES FILE
+
+}
