98233d00d11758352dcbe244c8a3a2a21d2cb3c2
Akbar <Akbar@cybertekschool.com>
2020-11-28 11:37:25 -0500

    JDBC Day4 - Removed hard-coded data from createConnection method , added properties for db url , username , password

--- src/test/java/utility/DB_Utility.java
+++ src/test/java/utility/DB_Utility.java
@@ -11,9 +11,9 @@ public class DB_Utility {
 
     public static void createConnection() {
 
-        String connectionStr = "jdbc:oracle:thin:@52.201.187.226:1521:XE";
-        String username = "hr";
-        String password = "hr";
+        String connectionStr = ConfigurationReader.getProperty("database.url");
+        String username = ConfigurationReader.getProperty("database.username");
+        String password = ConfigurationReader.getProperty("database.password");
 
         try {
             conn = DriverManager.getConnection(connectionStr, username, password);
