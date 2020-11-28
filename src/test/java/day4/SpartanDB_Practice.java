083d17937038151c7da3e2ee95b27e8854407f43
Akbar <Akbar@cybertekschool.com>
2020-11-28 15:08:55 -0500

    JDBC Day4 - Testing out SpartanDB

--- src/test/java/day4/SpartanDB_Practice.java
+++ src/test/java/day4/SpartanDB_Practice.java
@@ -1,13 +1,33 @@
 package day4;
 
+import utility.DB_Utility;
+
 public class SpartanDB_Practice {
 
     // create a main method
-    // call your create connection method with 3 params
-    // provide same connection string as HR
-    // provide username and password as SP SP
-    // run query SELECT * FROM SPARTANS
-    // call few methods from DB_Utility
+    public static void main(String[] args) {
+
+        String connectionStr = "jdbc:oracle:thin:@52.201.187.226:1521:XE" ;
+        String username = "SP";
+        String password = "SP";
+        // call your create connection method with 3 params
+        // provide same connection string as HR
+        // provide username and password as SP SP
+        DB_Utility.createConnection(connectionStr , username,password);
+
+        // run query SELECT * FROM SPARTANS
+        DB_Utility.runQuery("SELECT * FROM SPARTANS");
+        // call few methods from DB_Utility
+        // print out row number
+        System.out.println("DB_Utility.getRowCount() = " + DB_Utility.getRowCount());
+
+
+        DB_Utility.destroy();
+
+
+    }
+
+
 
     // after you are done with this :
     // now add below to your properties file
